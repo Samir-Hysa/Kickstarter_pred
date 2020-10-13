4fcad14200bd1394ee5bc8cab2621d9b9fe18ed7
Samir Hysa <samir.hysa.work@gmail.com>
2020-10-14 01:30:24 +0200

    updating everything

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,13 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# setting up pandas for a better data visualization
+plt.rc("font", size=14)
+sns.set(style="white")
+sns.set(style="whitegrid", color_codes=True)
+
+# setting up pandas for a better data visualization in terminal
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
@@ -8,7 +15,5 @@ pd.set_option('display.width', None)
 # importing data and read them
 data_path = "data /heart_failure_clinical_records_dataset.csv"
 data = pd.read_csv(data_path)
-print(data['age'])
-print(type(data['age']))
-
+print(data.head())
 
